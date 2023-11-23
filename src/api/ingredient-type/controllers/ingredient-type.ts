ac1106b47d29b313bf9d99d163bf1412ed07c29c
Fabien Winkler <fabien.winkler@outlook.fr>
2023-11-23 23:08:45 +0100

    initial commit

---
+++ src/api/ingredient-type/controllers/ingredient-type.ts
@@ -0,0 +1,7 @@
+/**
+ * ingredient-type controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ingredient-type.ingredient-type');
---
+++ src/api/ingredient-type/routes/ingredient-type.ts
@@ -0,0 +1,7 @@
+/**
+ * ingredient-type router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::ingredient-type.ingredient-type');
